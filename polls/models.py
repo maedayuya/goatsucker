ca0739dc34d9de960625b8e08f0b04bfb1ac9b59
maedayuya <maeda.yuya.infratop@gmail.com>
2021-12-16 21:25:24 +0900

    管理画面からライブ情報投稿機能追加

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date = models.DateField()
+    title = models.CharField(max_length=200)
+    open = models.TimeField()
+    start = models.TimeField()
+    ticket = models.CharField(max_length=200)
+    place = models.CharField(max_length=200)
+    link = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
