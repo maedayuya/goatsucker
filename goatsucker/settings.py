5a5411a940dcc282bbc28ae57b2bc1ae96625523
maedayuya <maeda.yuya.infratop@gmail.com>
2022-02-06 15:51:13 +0900

    デプロイ

--- goatsucker/settings.py
+++ goatsucker/settings.py
@@ -11,6 +11,21 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+...
+import django_heroku
+...
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+...
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    ...
+]
+...
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
