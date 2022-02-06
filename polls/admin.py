d4563701e1a4a943464098b4a399a663bb14f08f
maedayuya <maeda.yuya.infratop@gmail.com>
2022-02-06 15:10:20 +0900

    Twitterタイムライン表示&お問い合わせ先追加

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from .models import Post
 from .models import About
 from .models import Member
+from .models import Goods
 
 admin.site.register(Post)
 admin.site.register(About)
 admin.site.register(Member)
+admin.site.register(Goods)
+
--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from .models import About
 from .models import Member
+from .models import Goods
 
 from polls import models
 # Create your views here.
@@ -10,7 +11,8 @@ def index(request):
     context = {
         'data': Post.objects.filter().latest('created_date'),
         'about' : About.objects.get(id=1),
-        'member' : Member.objects.all()
+        'member' : Member.objects.all(),
+        'goods' : Goods.objects.all()    
     }
     
     return render(request, 'polls/index.html',context)
